99238b5bba751ee0541f404c83633c90f28534e0
sathyamus <sathyamus.sify@gmail.com>
2022-02-24 21:28:14 +0530

    (feat): In-case spring context not required

--- src/test/java/com/snsystems/cucumber/bdd/CucumberSpringConfiguration.java
+++ src/test/java/com/snsystems/cucumber/bdd/CucumberSpringConfiguration.java
@@ -4,7 +4,7 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfiguration {
 
 }
