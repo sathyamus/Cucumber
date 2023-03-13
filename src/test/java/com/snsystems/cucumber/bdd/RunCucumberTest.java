beabf33f9d3a339f534b2df2fd7758bf32d37063
sathyamus <sathyamus.sify@gmail.com>
2023-03-13 10:18:11 +0530

    (feat): cucumber html or json report

--- src/test/java/com/snsystems/cucumber/bdd/RunCucumberTest.java
+++ src/test/java/com/snsystems/cucumber/bdd/RunCucumberTest.java
@@ -4,13 +4,13 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
 //        tags = "@login",
-        plugin = {"pretty", "json:target/cucumber.json"})
+//        plugin = {"pretty", "json:target/cucumber.json"})
+        plugin = {"pretty", "html:target/cucumber.html"})
 //@ActiveProfiles("reg-test")
 //@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
 public class RunCucumberTest {
